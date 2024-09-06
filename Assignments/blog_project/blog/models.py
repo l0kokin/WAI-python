e626ff2cf07264afb5e531c87c81abb645550c8f
l0kokin <sali.gogishvili1@gmail.com>
2024-09-06 19:46:47 +0400

    completed assignment N7

--- Assignments/blog_project/blog/models.py
+++ Assignments/blog_project/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# from datetime import date
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    category = models.CharField(max_length=200)
