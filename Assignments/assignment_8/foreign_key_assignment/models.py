6cbcbde5e962f7ec47ba6f09859044e8b9cf8a86
l0kokin <sali.gogishvili1@gmail.com>
2024-09-10 11:18:30 +0400

    add student and course models, create examples to test functionality

--- Assignments/assignment_8/foreign_key_assignment/models.py
+++ Assignments/assignment_8/foreign_key_assignment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+# Task 1
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -18,3 +19,25 @@ class Book(models.Model):
 
     def _str_(self):
         return self.title
+
+
+# Task 2
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    grade = models.IntegerField()
+
+    def _str_(self):
+        return self.full_name
+
+    def get_courses(self):
+        return Course.objects.filter(students=self)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    students = models.ManyToManyField(Student)
+
+    def _str_(self):
+        return self.name
