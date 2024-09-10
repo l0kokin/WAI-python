631d4e6f12be654da5f1d5e44b8b967c3985da25
l0kokin <sali.gogishvili1@gmail.com>
2024-09-10 10:55:42 +0400

    create Author and Book methods and make sure the database works properly

--- Assignments/assignment_8/foreign_key_assignment/models.py
+++ Assignments/assignment_8/foreign_key_assignment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.name
+
+    def num_books(self):
+        return self.book_set.count()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    date_published = models.DateField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.title
