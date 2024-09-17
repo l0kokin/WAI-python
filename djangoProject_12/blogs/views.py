489fb6b318a040859891f90e950b74711d7ec3eb
l0kokin <sali.gogishvili1@gmail.com>
2024-09-17 11:48:26 +0400

    add html templates directory

--- djangoProject_12/blogs/views.py
+++ djangoProject_12/blogs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blogs.models import Blog
+
+
+def blogs_list(request):
+    # blogs = Blog.objects.all()
+    # context = {'blogs': blogs}
+    return render(request, 'blogs/blogs_list.html')
