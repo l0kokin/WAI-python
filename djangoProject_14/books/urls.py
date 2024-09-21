856e926241f9e173126cefeb50bcce9f37f5c595
l0kokin <sali.gogishvili1@gmail.com>
2024-09-21 14:13:35 +0400

    add book update view

--- djangoProject_14/books/urls.py
+++ djangoProject_14/books/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create/', views.create_book, name='books-create'),
     path('', views.list_books, name='books-list'),
     path('<int:pk>/', views.detail_book, name='books-detail'),
+    path('<int:pk>/update/', views.update_book, name='books-update'),
 ]
--- djangoProject_14/books/views.py
+++ djangoProject_14/books/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from books.forms import BookForm
 from books.models import Book
@@ -23,5 +23,17 @@ def list_books(request):
 
 
 def detail_book(request, pk):
-    book = Book.objects.get(pk=pk)
-    return render(request, )
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'books/books_detail.html', {'book': book})
+
+
+def update_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Success')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/books_update.html', {'form': form})
