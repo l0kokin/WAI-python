45c2f95405d544db6d985ac1a12005f2be3a3e15
l0kokin <sali.gogishvili1@gmail.com>
2024-09-21 13:54:20 +0400

    add books list

---
+++ djangoProject_14/books/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from books import views
+
+urlpatterns = [
+    path('create/', views.create_book, name='books-create'),
+    path('', views.list_books, name='books-list'),
+]
--- djangoProject_14/books/views.py
+++ djangoProject_14/books/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from books.forms import BookForm
+from books.models import Book
+
+
 # Create your views here.
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Succcess')
+        else:
+            form = BookForm()
+            return render(request, 'books/books_create.html', {'form': form})
+
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'books/books_list.html', {'books': books})
