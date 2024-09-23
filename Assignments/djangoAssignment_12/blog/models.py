57c5d3051856519d0b0825900b2ae7bce46462d5
l0kokin <sali.gogishvili1@gmail.com>
2024-09-23 17:52:02 +0400

    add template for posts

--- Assignments/djangoAssignment_12/blog/models.py
+++ Assignments/djangoAssignment_12/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_published = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
