820bb08064863b8adce76456f714b569060c3881
l0kokin <sali.gogishvili1@gmail.com>
2024-10-08 11:05:22 +0400

    add blogs author set

--- djangoProject_17/blogs/serializer.py
+++ djangoProject_17/blogs/serializer.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
 from blogs.models import Blogs
+from users.serializers import UserSerializer
 
 
 class BlogsSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
     class Meta:
         model = Blogs
         fields = ('id', 'title', 'content', 'author')
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        validated_data['author'] = request.user
+        return super().create(validated_data)
