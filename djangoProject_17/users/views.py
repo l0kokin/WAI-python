6ca9b6a3624a966a702d72a3865dc5842e453971
l0kokin <sali.gogishvili1@gmail.com>
2024-10-08 11:11:57 +0400

    final commit of project 17

--- djangoProject_17/users/views.py
+++ djangoProject_17/users/views.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
 from users.serializers import  UserSerializer
 
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
 
 class UserDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
